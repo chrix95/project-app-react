075e94ef7b89d17a12af86c348f15769f9db8325
chrix95 <31165216+chrix95@users.noreply.github.com>
2020-06-01 04:05:46 +0100

    Completed the loading of products and displaying as card

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Product from './views/Product'
 
 function App() {
   return (
-    <div>
+    <div className="relative pb-10 min-h-screen">
       <Router>
         <Header />
         <div className="p-3">
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,56 @@
-import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import React, { useState, useEffect } from 'react'
+import Loader from '../components/Loader'
+import ProductCard from '../components/ProductCard'
+
+import axios from 'axios'
 
 function Home () {
+  const url = `https://5ed45e9bfffad10016056ce0.mockapi.io/api/v2/posts?page=1&limit=10`
+  const [products, setProducts] = useState({ loading: false, data: null, error: false })
+
+  useEffect(() => {
+    setProducts({
+      loading: true,
+      data: null,
+      error: false
+    })
+    axios.get(url)
+    .then((res) => {
+      setProducts({
+        loading: false,
+        data: res.data,
+        error: false
+      })
+    })
+    .catch(() => {
+      setProducts({
+        loading: false,
+        data: null,
+        error: true
+      })
+    })
+  }, [url])
+
+  let content = null
+
+  if(products.error) {
+    content = <p>Error fetching products, please reload page</p>
+  }
+
+  if(products.loading) {
+    content = <Loader />
+  }
+
+  if(products.data) {
+    content = 
+      products.data.map((product) => 
+        <ProductCard key={product.id} product={product}/>
+      )
+  }
   return (
     <div>
-      <h1 className="font-bold text-2xl">This is the homepage</h1>
-      <HelloWorld name="devChris" />
+      <h1 className="font-bold text-2xl mb-3">Best Sellers</h1>
+      {content}
     </div>
   )
 }
