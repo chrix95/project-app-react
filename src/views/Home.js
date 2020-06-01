73ae0e5437dff168521f5e9315332c29d9c47a6b
chrix95 <31165216+chrix95@users.noreply.github.com>
2020-06-01 08:23:29 +0100

    Removed unused imports

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Loader from '../components/Loader'
 import ProductCard from '../components/ProductCard'
 import { useAxiosGet } from '../Hooks/HttpRequests'
